6b9de2f5301729d6d8ff264fda233b5827362a52
MARUF274 <he.gamer454@gmail.com>
2021-11-05 04:16:26 +0700

    mbuh

---
+++ main.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="card mb-3 mr-auto ml-auto border-info" style="max-width: 1200px;">
+        <div class="card-header text-center">
+            <h2>DAFTAR BUKU </h2>
+        </div>
+        <div class="row no-gutters">
+            <ul class="nav col-md-12 my-3 ">
+                <a href="add.php" class="btn btn-primary mx-3">Tambah</a>
+
+                <form class="form-inline ml-auto mx-4" style="margin-right: 2.1rem!important;">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </ul>
+            <div class="col-md-3 mx-3 my-3">
+                <div class="card col-md-10" style="">
+                    <h5 class="my-2">test</h5>
+                    <img src="https://via.placeholder.com/250x300" class="card-img-top my-3" alt="...">
+                        <div class="my-2">
+                        <a href="#" class="btn btn-warning col-md-5">Edit</a>
+                        <a href="#" class="btn btn-danger col-md-5">Delete</a>
+                        </div>
+                </div>
+            </div>
+                       
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
